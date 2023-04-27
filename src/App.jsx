019a76a94d93d2b7e3cc561101959dc0091ca370
nhat <nhatthuynhh621@gmail.com>
2023-04-27 14:32:28 +0700

    setup eslint prettier tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-400">
+      <h1 className="text-purple-300 font-bold">Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
