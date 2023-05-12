2012c46ca758c6480a7dc69b2c70fa9c87d4aec9
nhat <nhatthuynhh621@gmail.com>
2023-05-12 15:04:46 +0700

    fix task field warning when empty, update ui input restrict

---
+++ src/constant/task.constant.js
@@ -0,0 +1,5 @@
+export default {
+  EMPTY_TASK_FIELD: "Task field cannot be empty",
+  INVALID_DUE_DATE: "Invalid due date",
+  INVALID_PRIORITY: "Invalid priority",
+};
--- src/pages/Homepage/components/Task.jsx
+++ src/pages/Homepage/components/Task.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 import { MdModeEditOutline } from "react-icons/md";
 import { BsTrashFill } from "react-icons/bs";
 
@@ -10,6 +11,7 @@ import {
   deleteTaskFirebase,
 } from "../task.slice";
 import { getYYYYMMDDString } from "../../../utils/format.utils";
+import taskConstant from "../../../constant/task.constant";
 
 export default function Task({
   id,
@@ -31,6 +33,42 @@ export default function Task({
 
   const taskStyleCompleted = "line-through ml-2 h-fit";
   const taskStyleIncompleted = "ml-2 h-fit";
+
+  function handleUpdateTaskDueDate(e) {
+    const minDueDate = new Date("2000-01-01 00:00:00");
+    const maxDueDate = new Date("2100-01-01 00:00:00");
+    const inputDueDate = new Date(e.target.value);
+    inputDueDate.setHours(0);
+    if (
+      minDueDate.getTime() > inputDueDate.getTime() ||
+      maxDueDate.getTime() < inputDueDate.getTime()
+    ) {
+      toast(taskConstant.INVALID_DUE_DATE, { type: "error" });
+      return;
+    }
+    dispatch(
+      updateDueDateTaskFirebase({
+        email: userData.email,
+        taskid: id,
+        dueDate: e.target.value,
+      })
+    );
+  }
+
+  function handleUpdateTaskPriority(e) {
+    const priorityList = [1, 2, 3, 4];
+    if (!priorityList.includes(Number(e.target.value))) {
+      toast(taskConstant.INVALID_PRIORITY, { type: "error" });
+      return;
+    }
+    dispatch(
+      updateTaskPriorityFirebase({
+        email: userData.email,
+        taskid: id,
+        priority: e.target.value,
+      })
+    );
+  }
 
   return (
     <div className={timeLeft < 0 && !isCompleted ? dueTask : normalTask}>
@@ -86,30 +124,18 @@ export default function Task({
               type="date"
               className="w-full bg-white"
               value={getYYYYMMDDString(dueDate)}
-              onChange={(e) =>
-                dispatch(
-                  updateDueDateTaskFirebase({
-                    email: userData.email,
-                    taskid: id,
-                    dueDate: e.target.value,
-                  })
-                )
-              }
+              min="2000-01-01"
+              max="2100-01-01"
+              onKeyDown={(e) => e.preventDefault()}
+              onChange={handleUpdateTaskDueDate}
+              required
             />
           </div>
           <div className="min-[770px]:ml-2 max-[769px]:mt-2 static max-[290px]:w-[65%] max-[769px]:w-[75%] min-[770px]:w-[40%] lg:w-[40%] xl:w-[40%] border border-black rounded-md px-2 py-1 bg-white">
             <select
               className="w-full bg-white"
               value={priority || 1}
-              onChange={(e) =>
-                dispatch(
-                  updateTaskPriorityFirebase({
-                    email: userData.email,
-                    taskid: id,
-                    priority: e.target.value,
-                  })
-                )
-              }
+              onChange={handleUpdateTaskPriority}
             >
               <option value="1">Priority 1</option>
               <option value="2">Priority 2</option>
